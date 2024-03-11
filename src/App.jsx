88d598946547cd2d92589f4798beed2336fe4210
Adam <adammiller.2000nj@gmail.com>
2024-03-11 15:46:45 -0400

    added and styled container components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Search from './Components/Search';
 import FoodList from './Components/FoodList';
 import Nav from './Components/Nav';
+import Container from './Components/Container';
+import InnerContainer from './Components/InnerContainer';
 import './App.css';
 
 function App() {
@@ -12,7 +14,11 @@ function App() {
     <div className="App">
       <Nav />
        <Search foodData={foodData} setFoodData={setFoodData} />
-       <FoodList foodData={foodData} />
+       <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+       </Container>
     </div>
   )
 }
---
+++ src/Components/Container.jsx
@@ -0,0 +1,8 @@
+import style from './container.module.css';
+
+export default function Container({ children }) {
+
+    return(
+    <div className={style.parentContainer}>{children}</div>
+    )
+};
